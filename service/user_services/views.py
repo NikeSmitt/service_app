c77d9fbbbaca3eea48d9f578dca18ddb6b5238c8
gr <grim322@bk.ru>
2023-10-06 14:01:59 +0300

    Day 3 - 1 - Optimazing ORM query

--- service/user_services/views.py
+++ service/user_services/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from user_services.models import Subscription
+from user_services.serializers import SubscriptionSerializer
+
+
+class SubscriptionViewSet(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all().select_related('plan', 'client', 'client__user').only(
+        'client__user__email',
+        'client__company_name',
+        'plan_id',
+    )
+    serializer_class = SubscriptionSerializer
